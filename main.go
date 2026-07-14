d4169a94e4e1f7e96707bf44fd6932363f60f572
gclinoz <t96994024@gs.ncku.edu.tw>
2026-07-14 14:10:43 +0800

    CH1-L4: Newlines

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strings"
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -12,15 +14,33 @@ const fileName = "./messages.txt"
 func main() {
 	f, err := os.Open(fileName)
 	if err != nil {
-		log.Fatal("Error when opening the file")
+		log.Fatalf("error when opening %s: %s\n", fileName, err)
 	}
+	defer f.Close()
 
-	container := make([]byte, 8)
+	currentLine := ""
 	for {
-		_, err = f.Read(container)
-		if err == io.EOF {
-			return
+		b := make([]byte, 8, 8)
+		n, err := f.Read(b)
+		if err != nil {
+			if currentLine != "" {
+				fmt.Printf("read: %s\n", currentLine)
+				currentLine = ""
+			}
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			fmt.Printf("fail to read content: %s\n", err.Error())
+			break
 		}
-		fmt.Printf("read: %s\n", container)
+		str := string(b[:n])
+		parts := strings.Split(str, "\n")
+
+		for i := 0; i < len(parts) - 1; i++ {
+			currentLine += parts[i]
+			fmt.Printf("read: %s\n", currentLine)
+			currentLine = ""
+		}
+		currentLine += parts[len(parts) - 1]
 	}
 }
