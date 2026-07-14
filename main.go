0db6f9167444a7a127804c8cd8e7b6acc21da6f8
gclinoz <t96994024@gs.ncku.edu.tw>
2026-07-14 15:22:19 +0800

    CH1-L5: Channel Refactor

--- main.go
+++ main.go
@@ -11,36 +11,47 @@ import (
 
 const fileName = "./messages.txt"
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(ch)
+
+		currentLine := ""
+		for {
+			b := make([]byte, 8)
+			n, err := f.Read(b)
+			if err != nil {
+				if currentLine != "" {
+					ch <- currentLine
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("fail to read content: %s\n", err.Error())
+				return
+			}
+			str := string(b[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts) - 1; i++ {
+				ch <- currentLine + parts[i]
+				currentLine = ""
+			}
+			currentLine += parts[len(parts) - 1]
+		}
+	}()
+	return ch
+}
+
 func main() {
 	f, err := os.Open(fileName)
 	if err != nil {
 		log.Fatalf("error when opening %s: %s\n", fileName, err)
 	}
-	defer f.Close()
 
-	currentLine := ""
-	for {
-		b := make([]byte, 8, 8)
-		n, err := f.Read(b)
-		if err != nil {
-			if currentLine != "" {
-				fmt.Printf("read: %s\n", currentLine)
-				currentLine = ""
-			}
-			if errors.Is(err, io.EOF) {
-				break
-			}
-			fmt.Printf("fail to read content: %s\n", err.Error())
-			break
-		}
-		str := string(b[:n])
-		parts := strings.Split(str, "\n")
-
-		for i := 0; i < len(parts) - 1; i++ {
-			currentLine += parts[i]
-			fmt.Printf("read: %s\n", currentLine)
-			currentLine = ""
-		}
-		currentLine += parts[len(parts) - 1]
+	ch := getLinesChannel(f)
+	for item := range ch {
+		fmt.Printf("read: %s\n", item)
 	}
 }
